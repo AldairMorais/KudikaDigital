a8be74fa3acdddad3d34b27978bc3d1da90f81f3
Aldair <aldaiirmoraiss@gmail.com>
2022-04-05 13:18:27 +0100

    WEB_SEO

--- js/script.js
+++ js/script.js
@@ -58,3 +58,88 @@ function bgChanger() {
 }
 
 window.addEventListener("scroll", bgChanger);
+
+
+var btnContainer = document.querySelector(".listamnu")
+var btns = btnContainer.getElementsByTagName("li")
+
+for (var i =0; i<btns.length; i++) {
+ 
+ btns[i].addEventListener('click', function(){
+    var current = document.getElementsByClassName("actived");
+    current[0].className = current[0].className.replace(" actived")
+    this.className += " actived"
+})
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+
+btn.addEventListener("click", () => {
+menu.classList.toggle("hidden");
+});
+
+
+
+});
+
+
+
+
+
+var typeText = document.querySelector(".typeText")
+
+
+var textToBeTyped = "kudikadigital.com"
+var index = 0, isAdding = true
+
+function playAnim() {
+setTimeout(function () {
+// set the text of typeText to a substring of
+// the textToBeTyped using index.
+typeText.innerText = textToBeTyped.slice(0, index)
+if (isAdding) {
+// adding text
+
+if (index > textToBeTyped.length) {
+// no more text to add
+
+isAdding = false
+//break: wait 2s before playing again
+setTimeout( function () {
+  playAnim()
+}, 2000)
+return
+} else {
+// increment index by 1
+index++
+} 
+if(index == textToBeTyped.length){
+ document.querySelector('.jhj').style.display = "block";
+ document.querySelector('.jhjj').style.display = "block";
+ document.getElementById('qwsda1').style.display = "none";
+}
+} else {
+// removing text
+if (index === 0) {
+// no more text to remove
+isAdding = true
+document.querySelector('.jhj').style.display = "none";
+document.querySelector('.jhjj').style.display = "none";
+} else {
+// decrement index by 1
+document.getElementById('qwsda1').style.display = "block";
+document.querySelector('.jhj').style.display = "none";
+document.querySelector('.jhjj').style.display = "none";
+index--
+}
+}
+// call itself
+playAnim()
+}, 120)
+}
+// start animation
+playAnim()
+
